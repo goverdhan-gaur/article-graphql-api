373b5bb33fdbf114b20d03e6846206a25bb02f54
Goverdhan Gaur <84145630+goverdhan-gaur@users.noreply.github.com>
2023-02-13 14:36:01 -0500

    fix: added introspection

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  introspection: true
 });
 
 await server.start();
@@ -30,4 +31,3 @@ app.use(
 );
 
 await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/graphql`);
